2cd40a89ae4ed8ff81d42dcdabd2b5b2ea749dde
RakhmetovNurbolat <rakhmetov_nurbolat@mail.ru>
2025-07-11 21:06:19 +0500

    var and commet

--- JavaScript Basics/varAndCommet.js
+++ JavaScript Basics/varAndCommet.js
@@ -1,2 +1,22 @@
 // var let const
+var name = 'Jack';
+console.log(name);
+name = 'Jane';
+console.log(name);
+var color;
+console.log(color);
+color = 'red';
+console.log(color);
 
+/******************************
+ JavaScript names can include 
+ letter, numbers, $, _
+ *******************************/
+//JavaScript variables names can include 
+//letter, numbers, $, _
+// JavaScript variables names can't start with numbers: 1color
+
+//mainColor - camelCase
+//my_main_color - snake case
+//my-main-color - kebab case
+//MyMainColor - pascal case
