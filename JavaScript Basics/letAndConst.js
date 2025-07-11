cff9ce65e2767da73433c237fabf7e66aba2650d
RakhmetovNurbolat <rakhmetov_nurbolat@mail.ru>
2025-07-11 21:17:07 +0500

    let and const

---
+++ JavaScript Basics/letAndConst.js
@@ -0,0 +1,40 @@
+// //  let const
+// let name = 'Jack';
+// console.log(name);
+// name = 'Jane';
+// console.log(name);
+// let color;
+// console.log(color);
+// color = 'red';
+// console.log(color);
+
+const name = 'Jack';
+console.log(name);
+// name = 'Jane'
+
+const color = 'red';
+console.log(color);
+
+const user = {
+   name: 'Jack',
+   gender: 'male',
+   age: 24
+}
+console.log(user);
+
+user.name = 'Jane';
+console.log(user);
+
+// user = {
+//    name: 'Adam',
+//    gender: 'male',
+//    age: 24
+// }
+
+const colors = ['red', 'blue', 'orange'];
+console.log(colors)
+colors.push('blue');
+console.log(colors);
+
+// colors = ['red'];
+
--- JavaScript Basics/varAndCommet.js
+++ JavaScript Basics/varAndCommet.js
@@ -1,9 +1,9 @@
 // var let const
-var name = 'Jack';
+let name = 'Jack';
 console.log(name);
 name = 'Jane';
 console.log(name);
-var color;
+let color;
 console.log(color);
 color = 'red';
 console.log(color);
