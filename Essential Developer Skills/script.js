256b0ee74b74187bdd3aa343d3d2d4b2837d5ce6
RakhmetovNurbolat <rakhmetov_nurbolat@mail.ru>
2025-07-17 22:36:37 +0500

    lesson two

--- Essential Developer Skills/script.js
+++ Essential Developer Skills/script.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const myVar = 'je;;';
+const myVar = 25;
 
 while (false) {}
 
 const calcAge = (birthYear) => 2025 - birthYear;
+
+console.log();
+
+console.log('');
+
+console.log(myVar);
