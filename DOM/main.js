375f33fc06b03eee26bf42fe6c8ebeeffa456ded
RakhmetovNurbolat <rakhmetov_nurbolat@mail.ru>
2025-07-19 23:03:59 +0500

    lesson one

---
+++ DOM/main.js
@@ -0,0 +1,10 @@
+'use strict';
+// DOM - Document object model
+// const text = document.querySelector('.guess-message').textContent;
+// console.log(text);
+
+// document.querySelector('.guess-message').textContent = 'WIN';
+// document.querySelector('.question').textContent = 7;
+// document.querySelector('.score').textContent = 11;
+// document.querySelector('.number-input').value = 10;
+// console.log(document.querySelector('.number-input').value);
